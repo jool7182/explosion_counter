e2c4c0dab5817bf9f411662d90c3da2b6897eae8
jool7182 <131209371+jool7182@users.noreply.github.com>
2023-04-18 23:33:16 +0200

    Add files via upload

---
+++ explosion_counter.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+#gets html tags, finds <a> tags
+html_text = requests.get('https://en.wikipedia.org/wiki/List_of_explosions#Prior_to_2000').text
+soup = BeautifulSoup(html_text, 'lxml')
+sites = (soup.find_all('a'))
+
+#puts tags in a list
+result = []
+for country in sites:
+    a = str(country.get('href'))
+    result.append(a)
+
+#Converts tags to strings
+webs = ''
+for item in result:
+    webs = webs.lower() + item + '\n'
+
+#Regex to remove unnecessary string elements
+sub_str = '#cite_note-459'
+new_string = webs.split(sub_str)
+product = new_string[0]+sub_str
+
+#Gets user input and counts
+country = input('Type the name of a country to see how many major explosions it has experienced: ')
+country = country.lower()
+final = product.count(country)
+if final > 1 or final < 1:
+    print(f'{country.title()} has experienced {final} major explosions')
+else:
+    print(f'{country.title()} has experienced {final} major explosion')
+
+
+#file = open('explosions.txt', 'w')
+#file.write(product)
+#file.close()
+
